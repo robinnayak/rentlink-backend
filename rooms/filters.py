20714eee1ac43c696a15f151b8e59aac3ef2678d
robinnayak <robinnayak86@gmail.com>
2024-10-25 15:02:38 +0545

    location filters updated

--- rooms/filters.py
+++ rooms/filters.py
@@ -5,6 +5,8 @@ from .models import Room
 class RoomFilter(FilterSet):
     min_price = NumberFilter(field_name="price", lookup_expr='gte')
     max_price = NumberFilter(field_name="price", lookup_expr='lte')
+    province = CharFilter(field_name="province", lookup_expr='icontains')
+    district = CharFilter(field_name="district", lookup_expr='icontains')
     address = CharFilter(field_name="address", lookup_expr='icontains')
     is_available = BooleanFilter(field_name="is_available")
     has_water_supply = BooleanFilter(field_name="has_water_supply")
@@ -14,5 +16,5 @@ class RoomFilter(FilterSet):
 
     class Meta:
         model = Room
-        fields = ['min_price', 'max_price', 'address', 'is_available', 'has_water_supply', 'has_electricity', 'has_parking', 'has_wifi']
+        fields = ['min_price', 'max_price','province','district', 'address', 'is_available', 'has_water_supply', 'has_electricity', 'has_parking', 'has_wifi']
 
