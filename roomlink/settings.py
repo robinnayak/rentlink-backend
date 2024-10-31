de76734faa1cd8fc02972503703c4039e2228f75
robinnayak <robinnayak86@gmail.com>
2024-10-31 10:50:38 +0545

    token refresh days upgraded

--- roomlink/settings.py
+++ roomlink/settings.py
@@ -146,8 +146,8 @@ REST_FRAMEWORK = {
 
 # JWT Authentication Settings
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=8),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=8),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
